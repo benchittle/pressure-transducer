1472207f88c067dcf9f0562a3d1fa084a859d8cb
benchittle <72046603+benchittle@users.noreply.github.com>
2021-05-04 02:37:16 -0400

    Create rtc_testing.ino

---
+++ code/rtc_testing/rtc_testing/rtc_testing.ino
@@ -0,0 +1,12 @@
+#include <Wire.h>
+#include "RTClib.h"
+
+void setup() {
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   if (rtc.begin()) {
+    Serial.println("Found"
+  }
+}
