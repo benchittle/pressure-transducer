0e1840257cbebda3baa4b01c4ca7e098070a91c9
benchittle <72046603+benchittle@users.noreply.github.com>
2021-05-18 15:22:44 -0400

    finished documenting code

--- code/pressure_transducer/pressure_transducer.ino
+++ code/pressure_transducer/pressure_transducer.ino
@@ -4,15 +4,16 @@
 #include <avr/sleep.h>
 #include <avr/wdt.h>
 
-#include "RTClib.h"
-#include "SparkFun_MS5803_I2C.h"
+#include "RTClib.h" // https://github.com/adafruit/RTClib
+#include "SparkFun_MS5803_I2C.h" // https://github.com/sparkfun/SparkFun_MS5803-14BA_Breakout_Arduino_Library
 
 // The device will wait until this minute value to begin. Set a value less than
 // 0 to sample right away.
 #define START_MINUTE -1
 // Length of time to sample for.
 #define DATA_DURATION 1
-// Length of time to sleep after sampling for the above length of time.
+// Length of time to sleep after sampling for the above length of time. Set to
+// 0 for continuous sampling.
 #define SLEEP_DURATION 1
 // Number of samples to take per second (NOT IMPLEMENTED)
 #define SAMPLES_PER_SECOND 1
@@ -55,7 +56,10 @@ volatile bool active = true;
 // but not sampling, then it will go into a light sleep.
 volatile bool sampling = true;
 
-
+/*
+ * Setup the ATMEGA328P and peripherals to make sure connections are correct.
+ * We will also wait before baginning sampling until the specified START_MINUTE.
+ */
 void setup() {
 // Code between these if statements will only be run if ECHO_TO_SERIAL is not 0.
 #if ECHO_TO_SERIAL
@@ -69,7 +73,7 @@ void setup() {
   // Disable the Analog to Digital Converter to save power, as we don't use it
   // here (the MS5803 uses its own).
   ADCSRA = 0;
-  wdt_disable();
+  wdt_disable(); // Also disable the watchdog timer.
 
   // Test connection with the RTC. If it fails, then the error LED will blink
   // once per second until the device is restarted.
@@ -79,10 +83,11 @@ void setup() {
 #endif
     error(1);
   }
+
   // If the RTC lost power and therefore lost track of time, it will light up 
   // the LED for 2 seconds. While it is not necessary that the RTC has the right
-  // time, if multiple sensors are being deployed and are set to start all at the
-  // same time then this will warn the user that the sensors are not synchronized.
+  // time, if multiple sensors are being deployed and are meant to start at the
+  // same time, this will warn the user that the sensors are not synchronized.
   if (rtc.lostPower()) {
 #if ECHO_TO_SERIAL
     Serial.println(F("RTC time not configured (lost power)"));
@@ -97,9 +102,8 @@ void setup() {
   rtc.writeSqwPinMode(DS3231_OFF); // Square wave output
   rtc.clearAlarm(1); 
   rtc.disableAlarm(2);
-
-  // Test connection with the SD. If it fails, then the error LED will blink twice
-  // per second until the device is restarted.
+  // Test connection with the SD. If it fails, then the error LED will blink 
+  // twice per second until the device is restarted.
   if (!SD.begin(SD_CS_PIN)) {
 #if ECHO_TO_SERIAL
     Serial.println(F("SD setup error"));
@@ -112,10 +116,10 @@ void setup() {
   sensor.begin();
   
   now = rtc.now();
-  
-  // If a START_MINUTE value between 0 and 59 is set, the device will go into deep
-  // sleep until the current minute matches specified minute value. Otherwise, the
-  // device will start right away.
+
+  // If a START_MINUTE value between 0 and 59 is set, the device will go into 
+  // deep sleep until the current minute matches specified minute value.
+  // Otherwise, the device will start right away.
   if ((START_MINUTE >= 0) && (now.minute() != START_MINUTE)) {
 #if ECHO_TO_SERIAL
     Serial.println(F("Waiting for START_MINUTE"));
@@ -133,7 +137,17 @@ void setup() {
   enableTimer();
 }
 
-
+// =============================================================================
+/*
+ * Main program loop. First, the program determines whether a new day has 
+ * started. If so, it starts a new CSV file for data logging. Next, the device 
+ * will either be sampling or entering deep sleep. While sampling, the device 
+ * takes a specified number of samples per second, briefly sleeping between 
+ * samples, and saves them to the log file. This continues for a duration 
+ * specified by DATA_DURATION. While entering deep sleep, the device disables as
+ * many components as possible to save power and relies on an alarm pulse from 
+ * the DS3231 in order to wake up after a duration specified by SLEEP_DURATION
+ */
 void loop() {
   now = rtc.now();
 
@@ -144,8 +158,8 @@ void loop() {
     logfile.close();
     logfile = SD.open(now.toString(filename), FILE_WRITE);
 
-    // If there was an error creating the new log, the device will stop and blink
-    // three times per second until being restarted.
+    // If there was an error creating the new log, the device will stop and 
+    // blink three times per second until being restarted.
     if (!logfile) {
 #if ECHO_TO_SERIAL
       Serial.println(F("Couldn't create file."));
@@ -163,15 +177,15 @@ void loop() {
     oldDay = now.day();
   }
 
-  // If active, the device will see whether it's time to take a sample. Otherwise,
-  // enter deep sleep.
+  // If active, the device will see whether it's time to take a sample.
+  // Otherwise, enter deep sleep.
   if (active) {
     if (sampling) {
       char buffer[PRECISION + 6];
       double pressure = sensor.getPressure(ADC_4096);
       int temperature = sensor.getTemperature(CELSIUS, ADC_512); 
-      // Convert the pressure value to a string, as doubles are printed as ints
-      // to a logfile and we want to keep the fractional value.
+      // Convert the pressure value to a string, as doubles are printed as
+      // ints to a logfile and we want to keep the fractional value.
       dtostrf(pressure, 4, PRECISION, buffer);
           
       logfile.print(now.year());
@@ -216,7 +230,7 @@ void loop() {
     lightSleep();
 
   } else {
-    disableSampling();
+    disableTimer();
 
     // Enter deep sleep for the specified SLEEP_DURATION
     deepSleep(rtc.now() + TimeSpan(0, 0, SLEEP_DURATION, 0), DS3231_A1_Minute);
@@ -226,27 +240,38 @@ void loop() {
   }
 }
 
+// =============================================================================
 
 /*
  * Puts the device to sleep until being awakened by the RTC's alarm, which
- * will be detected when pin D2 goes low. Arguments will be passed to (and
- * are the same as) rtc.setAlarm1.
+ * should pull pin D2 low and trigger an interrupt. Arguments will be passed to
+ * (and are the same as) rtc.setAlarm1. This sleep mode saves more power than
+ * lightSleep
  */
-void deepSleep(const DateTime dt, Ds3231Alarm1Mode alarm_mode) {
-  byte temp1 = 0;
-  byte temp2 = 0;
+void deepSleep(const DateTime wake_time, Ds3231Alarm1Mode alarm_mode) {
 #if ECHO_TO_SERIAL
   Serial.println(F("Entering deep sleep"));
   Serial.flush();
 #endif
-  rtc.setAlarm1(dt, alarm_mode);
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  // When the alarm goes off, it will cause the SQW pin of the DS3231 to go low.
+  rtc.setAlarm1(wake_time, alarm_mode);
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // Saves the most power.
+  // Disable interrupts while preparing to sleep. Without doing this, the
+  // interrupt could go off before the sleep_cpu() command and leave the device
+  // stuck in a sleep state with no interrupt to wake it up.
   noInterrupts();
   sleep_enable();
+  // When a low signal is detected on pin D2, the deepSleepISR will run as an
+  // interrupt service routine (ISR) to wake the device up.
   attachInterrupt(INTERRUPT_INTPIN, deepSleepISR, LOW);
-  sleep_bod_disable();
-  interrupts();
-  sleep_cpu();  
+  // Disable brown out detection to save power (must be done as the last 
+  // instruction before sleeping).
+  sleep_bod_disable(); 
+  interrupts(); // Enable interrupts again.
+  sleep_cpu(); // Enter sleep.
+
+  // When the device wakes up, it will resume here after executing the 
+  // deepSleepISR.
 
   rtc.clearAlarm(1);
 #if ECHO_TO_SERIAL
@@ -255,8 +280,9 @@ void deepSleep(const DateTime dt, Ds3231Alarm1Mode alarm_mode) {
 #endif
 }
 
+
 /*
- * 
+ * Interrupt Service Routine used to wake the device from deep sleep.
  */
 void deepSleepISR() {
   sleep_disable();
@@ -265,7 +291,51 @@ void deepSleepISR() {
 
 
 /*
- * 
+ * Begin using the 32KHz output of the DS3231 as an external clock source for
+ * the MCU's Timer/Counter2. This timer is then set to trigger interrupts at
+ * regular intervals, depending on the sampling frequency. We use the DS3231's
+ * 32KHz output since the MCU's timers are not as accurate on their own.
+ */
+void enableTimer() {
+  // Begin the clock on the DS3231, which should be connected to pin TOSC1/XTAL1.
+  rtc.enable32K();
+
+  // Switching to an asynchronous clock source as described in section 18.9 
+  // (Asynchronous Operation of Timer/Counter2) of the datasheet:
+
+  // Tell the MCU that we will be using an external clock source (EXCLK = 1) and
+  // that we want Timer/Counter2 to be clocked from a clock source connected to
+  // pin TOSC1/XTAL1 (AS2 = 1).
+  ASSR |= _BV(EXCLK) | _BV(AS2);
+  
+  TCCR2A = 0; // Using normal operation mode settings here.
+  // Set the prescaler value to clk/128. This will result in the timer 
+  // overflowing once per second.
+  TCCR2B = _BV(CS22) | _BV(CS20);
+  TCNT2 = 0; // Reset the timer to 0.
+  
+  // Wait for the above changes to be made.
+  while (ASSR & (_BV(TCN2UB) | _BV(TCR2AUB) | _BV(TCR2BUB)));
+  
+  TIFR2 = _BV(OCF2B) | _BV(OCF2A) | _BV(TOV2); // Clear the interrupt flags.
+
+  // Set the timer to create an interrupt whenever it overflows.
+  TIMSK2 = _BV(TOIE2);
+
+  // This will allow the main loop to execute the sampling code.
+  active = true;
+  sampling = true;
+#if DATA_DURATION > 0
+  // Set an interrupt and alarm to stop sampling after the time specified by
+  // DATA_DURATION has passed.
+  attachInterrupt(INTERRUPT_INTPIN, stopSamplingISR, LOW);
+  rtc.setAlarm1(rtc.now() + TimeSpan(0, 0, DATA_DURATION, 0), DS3231_A1_Minute);
+#endif
+}
+
+
+/*
+ * Interrupt Service Routine used to tell the device to stop sampling.
  */
 void stopSamplingISR() {
   detachInterrupt(INTERRUPT_INTPIN);
@@ -274,51 +344,25 @@ void stopSamplingISR() {
 
 
 /*
- * 
- */
-void enableTimer() {
-  // Switching to an asynchronous clock source as described in section 18.9 
-  // (Asynchronous Operation of Timer/Counter2) of the datasheet
-  TIMSK2 = 0;
-  
-  rtc.enable32K();
-  ASSR |= _BV(EXCLK) | _BV(AS2);
-  
-  TCCR2A = 0;
-  // Set the prescaler value to clk/128. This will result in the timer overflowing once per second.
-  TCCR2B = _BV(CS22) | _BV(CS20);
-  // Set the timer to 0.
-  TCNT2 = 0;
-  // Wait for the above changes to be made.
-  while (ASSR & (_BV(TCN2UB) | _BV(TCR2AUB) | _BV(TCR2BUB)));
-  // Clear the interrupt flags.
-  TIFR2 = _BV(OCF2B) | _BV(OCF2A) | _BV(TOV2);
-
-  // Set the timer to create an interrupt whenever it overflows.
-  TIMSK2 = _BV(TOIE2);
-
-  active = true;
-  sampling = true;
-
-  attachInterrupt(INTERRUPT_INTPIN, stopSamplingISR, LOW);
-  rtc.setAlarm1(rtc.now() + TimeSpan(0, 0, DATA_DURATION, 0), DS3231_A1_Minute);
-}
-
-void disableSampling() {
-  TIMSK2 = 0;
-  rtc.disable32K();
-  rtc.clearAlarm(1);
-}
-
-/*
- * 
+ * Interrupt Service Routine used wake the device up from light sleep and tell
+ * it to take a single sample.
  */
 ISR(TIMER2_OVF_vect) {
   sampling = true;
 }
 
+
+void disableTimer() {
+  TIMSK2 = 0; // Disable Timer/Counter2 interrupts.
+  rtc.disable32K();
+  rtc.clearAlarm(1);
+}
+
+
 /*
- * 
+ * Puts the device to sleep until being awakened by an interrupt from 
+ * Timer/Counter2. This sleep mode saves less power than deepSleep, but is
+ * faster to wake up from when higher frequency sampling is needed.
  */
 void lightSleep() {
 #if ECHO_TO_SERIAL
@@ -333,14 +377,20 @@ void lightSleep() {
 
   set_sleep_mode(SLEEP_MODE_PWR_SAVE);
   
-  // Prevent interrupts while getting ready to sleep. If an interrupt were to occur
-  // before sleeping, it would detatch and no longer run to wake up the device.
+  // Disable interrupts while preparing to sleep. Without doing this, the
+  // interrupt could go off before the sleep_cpu() command and leave the device
+  // stuck in a sleep state with no interrupt to wake it up.
   noInterrupts();
   sleep_enable();
+  // Disable brown out detection to save power (must be done as the last 
+  // instruction before sleeping).
   sleep_bod_disable();
-  interrupts();
-  sleep_cpu();
+  interrupts(); // Enable interrupts again.
+  sleep_cpu(); // Enter sleep.
   
+  // When the device wakes up, it will resume here after executing the 
+  // TIMER2_OVF_vect ISR.
+
   sleep_disable();
 
 #if ECHO_TO_SERIAL
@@ -349,8 +399,13 @@ void lightSleep() {
 #endif
 }
 
+
+/*
+ * Enter an endless loop while flashing the error LED a given number of times
+ * each second.
+ */
 void error(short flashes) {
-  int duration = 2000 / flashes;
+  int duration = 1000 / flashes;
   while (1) {
     for (uint8_t i = 0; i < flashes; i++) {
       digitalWrite(ERROR_LED_PIN, HIGH);
