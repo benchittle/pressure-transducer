ee409be58ab812c3e980394f703ba878449278ff
benchittle <72046603+benchittle@users.noreply.github.com>
2021-06-17 00:57:26 -0400

    Limit sampling duration to 60 minutes for now

--- code/DIY1/DIY1.ino
+++ code/DIY1/DIY1.ino
@@ -73,6 +73,7 @@ uint8_t oldDay = 0;
 // exists. Otherwise, they will be set to the corresponding default values 
 // above.
 uint16_t sleepDuration = DEFAULT_SLEEP_DURATION;
+uint16_t samplingDurationInt = DEFAULT_SAMPLING_DURATION;
 TimeSpan samplingDuration = TimeSpan(DEFAULT_SAMPLING_DURATION * 60);
 char infoString[INFO_STRING_SIZE] = DEFAULT_INFO_STRING;
 
@@ -190,7 +191,8 @@ void setup() {
 
     // Set these values based on the corresponding config values.
     startMinute = configVars[0]; // 1st line in file.
-    samplingDuration = TimeSpan(configVars[1] * 60); // 2nd line in file.
+    samplingDurationInt = configVars[1];
+    samplingDuration = TimeSpan(configVars[1] >= 60 ? 0 : configVars[1]); // 2nd line in file.
     sleepDuration = configVars[2]; // 3rd line in file.
   } else {
 #if ECHO_TO_SERIAL
@@ -203,7 +205,7 @@ void setup() {
   Serial.print(F("startMinute = "));
   Serial.println(startMinute);
   Serial.print(F("samplingDuration = "));
-  Serial.println(samplingDuration.minutes());
+  Serial.println(configVars[1]);
   Serial.print(F("sleepDuration = "));
   Serial.println(sleepDuration);
   Serial.print(F("infoString = "));
