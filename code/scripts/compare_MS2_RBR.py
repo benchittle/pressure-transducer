225351026ac32a588e444926ba944181d86ea764
benchittle <benchittle@me.com>
2021-08-12 09:59:33 -0400

    Finish plotting menu for now, drop duplicate times and interpolate missing ones in input data

--- code/scripts/compare_MS2_RBR.py
+++ code/scripts/compare_MS2_RBR.py
@@ -4,22 +4,24 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-PATH = "test_data\\"
+INPUT_PATH = r"C:\Users\Uni\Desktop\Ben\test" + "\\"
+OUTPUT_PATH = "output.xlsx"
 
-def read_ms2_csvs(path):
+def read_ms2_csvs(INPUT_PATH):
     
     csvs = []
-    for file_name in os.listdir(path):
-        head, extension = os.path.splitext(file_name)
+    for file_name in os.listdir(INPUT_PATH):
+        head, extension = os.INPUT_PATH.splitext(file_name)
         # Valid name example: S1_20210813-1345.csv
         if extension == ".csv":
             sensor, timestamp = head.split("_")
             data = pd.read_csv(
-                filepath_or_buffer=path + file_name, 
+                fileINPUT_PATH_or_buffer=INPUT_PATH + file_name, 
                 skiprows=2, 
                 usecols=["datetime", "pressure"], 
                 parse_dates=["datetime"], 
                 dayfirst=True)
+            data.drop_duplicates("datetime", inplace=True)
             data.set_index("datetime", inplace=True)
             data.rename(columns={"pressure" : sensor}, inplace=True)
             
@@ -29,11 +31,11 @@ def read_ms2_csvs(path):
 
 
 
-def read_rbr_xls(path):
-    for file_name in os.listdir(path):
+def read_rbr_xls(INPUT_PATH):
+    for file_name in os.listdir(INPUT_PATH):
         if file_name.endswith(".xls"):
             data = pd.read_excel(
-                io=path + file_name,
+                io=INPUT_PATH + file_name,
                 skiprows=5, 
                 usecols=[0, 2], # Select Timestamp and Pressure columns
                 names=["datetime", "pressure"]) # Rename columns
@@ -77,20 +79,20 @@ def get_valid_int(prompt):
 
 
 def main():
-    start_time = "2021-08-08 17:16:00"
-    end_time = "2021-08-08 17:29:00"
+    start_time = "2021-08-10 20:21:30"
+    end_time = "2021-08-10 20:36:50"
 
     # Read RBR pressure data.
-    rbr_raw = read_rbr_xls(PATH)
+    rbr_raw = read_rbr_xls(INPUT_PATH)
     # Read all collected pressure data from sensors.
-    sensors_raw = read_ms2_csvs(PATH)
+    sensors_raw = read_ms2_csvs(INPUT_PATH)
 
     # DataFrame for storing aggreate statistics about sensor performance.
     sensor_stats = pd.DataFrame(columns=sensors_raw.columns)
 
     # Get the data from during the testing period.
     rbr = rbr_raw.loc[start_time:end_time]
-    sensors = sensors_raw.loc[start_time:end_time]
+    sensors = sensors_raw.loc[start_time:end_time].interpolate(limit=2)
 
     # Calculate the residuals for each sensors (MS2 - RBR).
     residuals = sensors.subtract(rbr, axis=0)
@@ -117,29 +119,39 @@ def main():
     # sensors against the RBR.
     sensor_stats.loc["r squared"] = sensors_corrected.corrwith(rbr) ** 2
 
-    all_raw = pd.concat([sensors_raw, rbr_raw], axis=1)
+    print("\n\nSTATS")
+    print(sensor_stats)
+    print("\n")
     
     menu_string = ("Plotting:\n"
             "1. Plot the pressure curves of all sensors\n"
-            "2. Plot a sensor's corrected pressure (x axis) against the RBR (y axis)\n"
-            "3. Show plots\n"
-            "4. Quit\n")
+            "2. Plot the pressure curves of all sensors within the specified time range\n"
+            "3. Plot a sensor's corrected pressure (x axis) against the RBR (y axis)\n"
+            "4. Show plots\n"
+            "5. Quit\n")    
     
     while True:
         action = get_valid_int(menu_string)
             
         if action == 1:
-            plot_compare(all_raw)
+            plot_compare(pd.concat([rbr_raw, sensors_raw], axis=1))
         elif action == 2:
+            plot_compare(pd.concat([rbr, sensors], axis=1))
+        elif action == 3:
             while True:
                 try:
-                    plot_against(rbr, sensors_corrected["S" + str(get_valid_int())])
+                    plot_against(rbr, sensors_corrected["S" + str(get_valid_int())] )
                     break
                 except IndexError:
                     print("INVALID INPUT: There is no data for that sensor")
+        elif action == 4:
+            print("Showing plots\n")
+            plt.show()
+        elif action == 5:
+            print("Quitting")
+            break
         else:
             print("INVALID INPUT: Enter an integer between 1 and 4")
-        
         
 
 if __name__ == "__main__":
