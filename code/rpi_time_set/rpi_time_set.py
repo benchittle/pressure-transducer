d0302de21a0cd142e789900a7d268c99775cc95a
Ben Chittle <benchittle@me.com>
2021-08-26 15:03:43 -0400

    Change to output old and new RTC time just once

--- code/rpi_time_set/rpi_time_set.py
+++ code/rpi_time_set/rpi_time_set.py
@@ -3,16 +3,21 @@ import SDL_DS3231 # See file for details. GitHub repo: https://github.com/switch
 
 
 rtc = SDL_DS3231.SDL_DS3231(1, 0x68)
+
+old = rtc.read_datetime()
 rtc.write_now()
+now = rtc.read_datetime()
+print("RTC time was: {}".format(old))
+print("RTC time now: {}\n\n".format(now))
 
 
 t = time.perf_counter() + 1
 
-
+"""
 while True:
     if time.perf_counter() > t:
         t = time.perf_counter() + 1
-        
-        print("Time now: {}".format(time.strftime("%Y:%m:%d %H:%M:%S")))
+
+        print("System time: {}".format(time.strftime("%Y:%m:%d %H:%M:%S")))
         print("RTC Time: {}\n".format(rtc.read_datetime()))
- 
+""" 
