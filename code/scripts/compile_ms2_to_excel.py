d74718ec2ea5fb33902e2325100eb0120b0fad85
benchittle <72046603+benchittle@users.noreply.github.com>
2021-09-08 20:10:34 -0400

    Add comments

--- code/scripts/compile_ms2_to_excel.py
+++ code/scripts/compile_ms2_to_excel.py
@@ -23,6 +23,7 @@ DAY3_TRANSECTS = [
 
 def read_ms2_csvs(input_path):
     csvs = []
+    # Read each csv in the given directory as sensor data.
     for file_name in os.listdir(input_path):
         head, extension = os.path.splitext(file_name)
         # Valid name example: S1_20210813-1345.csv
@@ -30,14 +31,20 @@ def read_ms2_csvs(input_path):
             sensor, timestamp = head.split("_")
             if len(sensor) == 2:
                 sensor = "S0" + sensor[-1]
+            # Read the datetime and pressure columns.
             data = pd.read_csv(
                 filepath_or_buffer=input_path + file_name, 
                 skiprows=2, 
                 usecols=["datetime", "pressure"], 
                 parse_dates=["datetime"], 
                 dayfirst=True)
+            # Every so often two readings occur in the same second. It's easier
+            # to work with the data if we drop these.
+            # ISSUE: THIS DROPS BOTH RATHER THAN JUST ONE OF THE DUPLICATES
             data.drop_duplicates("datetime", inplace=True)
             data.set_index("datetime", inplace=True)
+            # Rename the data's pressure column to the name of the sensor that 
+            # produced it.
             data.rename(columns={"pressure" : sensor}, inplace=True)
             
             csvs.append(data.squeeze())
@@ -45,15 +52,26 @@ def read_ms2_csvs(input_path):
     return pd.concat(csvs, axis=1)
 
 def main():
+    # Read sensor data into a single data frame with DateTimes in the index
+    # and pressure reading for each sensor as the columns.
     sensor_data = read_ms2_csvs(INPUT_PATH)
+    # Reorder the columns based on the order of the transects.
     sensor_data = sensor_data.reindex(DAY3_TRANSECTS, axis=1)
     
+    # Read in offset constants to adjust data.
     offsets = pd.read_csv(OFFSET_INPUT_PATH, usecols=["Sensor", "Offset"])
+    # Squeeze to series and drop offset values for sensors that are not present
+    # in the data.
     offsets = offsets.set_index("Sensor").squeeze()[sensor_data.columns]
 
+    # Write data to Excel:
     with pd.ExcelWriter(OUTPUT_PATH) as writer:
+        # Raw data
         sensor_data.to_excel(writer, "raw")
+        # Adjusted data (adding the corresponding offset to each pressure 
+        # reading)
         (sensor_data + offsets).to_excel(writer, "adjusted")
+        # Offset values used.
         offsets.to_excel(writer, "offsets")
 
     print("Done")
