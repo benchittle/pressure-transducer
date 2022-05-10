4687d8994a1e560c241585fe1d920207ed9eb423
Ben Chittle <benchittle@me.com>
2022-05-10 16:13:56 -0400

    Add basic ULP code using HULP library

--- code/DIY3-esp32/DIY3-esp32.ino
+++ code/DIY3-esp32/DIY3-esp32.ino
@@ -2,6 +2,7 @@
 #include <driver/adc.h>
 #include <esp_wifi.h>
 #include <esp_bt.h>
+#include <esp32/ulp.h>
 
 // Additional includes for peripheral devices
 #include <FS.h>
@@ -9,6 +10,8 @@
 #include <SPI.h>
 #include "RTClib.h"
 #include "MS5803_05.h"
+
+#include "hulp.h"
 
 #define ECHO_TO_SERIAL 1
 
@@ -21,6 +24,8 @@ DateTime now;
 RTC_DATA_ATTR MS_5803 sensor(4096); // MAYBE CAN LOWER OVERSAMPLING
 
 uint8_t oldDay = 0;
+
+RTC_DATA_ATTR ulp_var_t ulpNum;
 
 /*
 void printResetReason(esp_reset_reason_t reason) {
@@ -42,18 +47,31 @@ void printResetReason(esp_reset_reason_t reason) {
 
 
 void setup() {
+
+    const ulp_insn_t program[] = {
+        I_MOVI(R0, 0),
+        I_GET(R1, R0, ulpNum),
+        I_ADDI(R1, R1, 10),
+        I_PUT(R1, R0, ulpNum),
+
+        I_GPIO_SET(GPIO_NUM_26, 0),
+
+        I_END()
+    };
+
     #if ECHO_TO_SERIAL
         Serial.begin(115200);
         delay(1000);
+        Serial.printf("VAL: %d\n", ulpNum.val);
+        Serial.flush();
+    #else
+        // ADC, WiFi, BlueTooth are disabled by default.        
+        // Default CPU frequency is 240MHz, but we don't need high speed.
+        setCpuFrequencyMhz(10);     
     #endif
-
-    // ADC, WiFi, BlueTooth are disabled by default.        
-    // Default CPU frequency is 240MHz, but we don't need high speed.
-    setCpuFrequencyMhz(10);   
 
     pinMode(SD_CS_PIN, OUTPUT);
     pinMode(RTC_POWER_PIN, OUTPUT);
-
 
     switch (esp_reset_reason()) {    
         case ESP_RST_POWERON: {
@@ -69,6 +87,7 @@ void setup() {
             if (!rtc.begin()) {
                 #if ECHO_TO_SERIAL
                     Serial.println(F("RTC setup error"));
+                    Serial.flush();
                 #endif
             }
 
@@ -89,6 +108,7 @@ void setup() {
             if (!SD.begin(SD_CS_PIN)) {
                 #if ECHO_TO_SERIAL
                     Serial.println(F("SD setup error"));
+                    Serial.flush();
                 #endif
             }
 
@@ -96,28 +116,37 @@ void setup() {
             if (!sensor.initializeMS_5803(true)) {
                 #if ECHO_TO_SERIAL
                     Serial.println(F("MS5803-05 sensor setup error"));
+                    Serial.flush();
                 #endif
             }
             sensor.readSensor();
             Serial.println(sensor.pressure());
 
-            Serial.println("Done");
-
-            delay(1000);
             digitalWrite(RTC_POWER_PIN, LOW);
-            Wire.~TwoWire(); // essentially Wire.end(); MIGHT BE UNNECESSARY
-            esp_deep_sleep(1 * 1000000);
+            //Wire.~TwoWire(); // essentially Wire.end(); MIGHT BE UNNECESSARY
+
+            ulpNum.val = 0;
+            hulp_peripherals_on();
+            hulp_configure_pin(GPIO_NUM_26, RTC_GPIO_MODE_OUTPUT_ONLY, GPIO_FLOATING, 0);
+            ESP_ERROR_CHECK(hulp_ulp_load(program, sizeof(program), 1000000, 0));
+            ESP_ERROR_CHECK(hulp_ulp_run(0));
+            
+            esp_deep_sleep(8 * 1000000);
 
             break; // switch
         }
 
         case ESP_RST_DEEPSLEEP: {
             Serial.println("IM AWAAAAKE");
+            Serial.flush();
+
+            /*
             
             // The I2C pull-up resistors are on the DS3231 board which is pin
             // powered, so that pin must be powered for I2C to work with the 
             // MS5803.
             digitalWrite(RTC_POWER_PIN, HIGH);
+            delay(50);
 
             // I2C communication needs to be reinitialized after a deep sleep
             // reset. The sensor.initializeMS_5803() method could be used here
@@ -126,11 +155,17 @@ void setup() {
             sensor.resetSensor();      
                   
             sensor.readSensor();
+            Serial.print("Pressure: ");
             Serial.println(sensor.pressure());
             Serial.flush();
             digitalWrite(RTC_POWER_PIN, LOW);
+            */
 
-            esp_deep_sleep(10 * 1000000);
+            hulp_peripherals_on();
+            hulp_configure_pin(GPIO_NUM_26, RTC_GPIO_MODE_OUTPUT_ONLY, GPIO_FLOATING, 0);
+            ESP_ERROR_CHECK(hulp_ulp_run(0));
+
+            esp_deep_sleep(8 * 1000000);
 
             break; // switch
         }
@@ -138,6 +173,7 @@ void setup() {
         default: 
             #if ECHO_TO_SERIAL
                 Serial.println("RESET REASON CASE NOT HANDLED");
+                Serial.flush();
             #endif
     }
 }
