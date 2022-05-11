cfa775ce07fb5fa14a8064cdb824abdcde3ae0eb
Ben Chittle <benchittle@me.com>
2022-05-11 16:43:23 -0400

    Reinitialize RTC on wakeup (inefficient)

--- code/DIY3-esp32/DIY3-esp32.ino
+++ code/DIY3-esp32/DIY3-esp32.ino
@@ -19,30 +19,18 @@
 #define RTC_POWER_PIN D3
 
 
-RTC_DS3231 rtc;
-DateTime now;
+typedef struct {
+    uint32_t timestamp;
+    float pressure;
+    int8_t temperature;
+} /*__attribute__((packed))*/ entry_t;
 
 RTC_DATA_ATTR MS_5803 sensor(4096); // MAYBE CAN LOWER OVERSAMPLING
 
-uint8_t oldDay = 0;
+RTC_DATA_ATTR uint8_t oldDay = 0;
 
-/*
-void printResetReason(esp_reset_reason_t reason) {
-    switch (reason) {
-        case ESP_RST_UNKNOWN: Serial.println("unknown"); break;
-        case ESP_RST_POWERON: Serial.println("poweron"); break;
-        case ESP_RST_EXT: Serial.println("ext"); break;
-        case ESP_RST_SW: Serial.println("sw"); break;
-        case ESP_RST_PANIC: Serial.println("panic"); break;
-        case ESP_RST_INT_WDT: Serial.println("int_wdt"); break;
-        case ESP_RST_TASK_WDT: Serial.println("task wdt"); break;
-        case ESP_RST_WDT: Serial.println("wdt"); break;
-        case ESP_RST_DEEPSLEEP: Serial.println("deepsleep"); break; 
-        case ESP_RST_BROWNOUT: Serial.println("brownout"); break;
-        case ESP_RST_SDIO: Serial.println("sdio"); break;
-    }
-}
-*/
+RTC_DATA_ATTR entry_t buffer[200];
+
 
 void setup() {
     #if ECHO_TO_SERIAL
@@ -57,13 +45,17 @@ void setup() {
     pinMode(SD_CS_PIN, OUTPUT);
     pinMode(RTC_POWER_PIN, OUTPUT);
 
-    switch (esp_reset_reason()) {    
+    RTC_DS3231 rtc;
+    DateTime now;
+    switch (esp_reset_reason()) {   
+
         case ESP_RST_POWERON: {
             // Initialize the connection with the RTC:
             // Power to the RTC is provided by a GPIO pin. We need to wait a short 
             // duration (10ms seems to work) before connecting to the RTC or it fails.
             // This might be because it has to switch from battery power to VCC power.
             digitalWrite(RTC_POWER_PIN, HIGH);
+
             // According to the datasheet, the oscillator takes <1 sec to begin (on 
             // first time startup), so we wait 1 second in case this is the first time
             // startup. This accounts for the 10ms delay mentioned above, too.
@@ -74,19 +66,6 @@ void setup() {
                     Serial.flush();
                 #endif
             }
-
-            now = rtc.now();
-            Serial.print(now.year());
-            Serial.print(F("-"));
-            Serial.print(now.month());
-            Serial.print(F("-"));
-            Serial.print(now.day());
-            Serial.print(F(" @ "));
-            Serial.print(now.hour());
-            Serial.print(F(":"));
-            Serial.print(now.minute());
-            Serial.print(F(":"));
-            Serial.println(now.second());
 
             // Initialize the connection with the SD card.
             if (!SD.begin(SD_CS_PIN)) {
@@ -103,13 +82,11 @@ void setup() {
                     Serial.flush();
                 #endif
             }
-            sensor.readSensor();
-            Serial.println(sensor.pressure());
 
             digitalWrite(RTC_POWER_PIN, LOW);
             Wire.~TwoWire(); // essentially Wire.end(); MIGHT BE UNNECESSARY
 
-            esp_deep_sleep(8 * 1000000);
+            esp_deep_sleep(5 * 1000000);
 
             break; // switch
         }
@@ -127,16 +104,23 @@ void setup() {
             // I2C communication needs to be reinitialized after a deep sleep
             // reset. The sensor.initializeMS_5803() method could be used here
             // instead but it would add unecessary initialization steps.
-            Wire.begin();
+            // Wire.begin();  // <-- UNCOMMENT IF NOT USING rtc.begin() 
+
+            // Reinitialize rtc (also reinitializess I2C for the MS5803-05)
+            if (!rtc.begin()) {  // TODO: AVOID HAVING TO CALL THIS FUNCTION EVERY TIME BY STORING rtc IN RTC MEMORY
+                Serial.println("AHHHHHHHH");
+            }
+            now = rtc.now();
+            Serial.printf("%d-%d-%d @ %d:%d:%d\n", now.year(), now.month(), now.day(), now.hour(), now.minute(), now.second());
+
             sensor.resetSensor();      
                   
             sensor.readSensor();
-            Serial.print("Pressure: ");
-            Serial.println(sensor.pressure());
+            Serial.printf("Pressure: %f\n\n", sensor.pressure());
             Serial.flush();
             digitalWrite(RTC_POWER_PIN, LOW);
 
-            esp_deep_sleep(8 * 1000000);
+            esp_deep_sleep(5 * 1000000);
 
             break; // switch
         }
