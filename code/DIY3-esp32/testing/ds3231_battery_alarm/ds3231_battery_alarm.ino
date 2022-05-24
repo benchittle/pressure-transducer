df3c2dc0264383e54a38b27f14eeb3cae34a8121
Ben Chittle <benchittle@me.com>
2022-05-24 14:59:09 -0400

    Modify to trigger deep sleep wakeups

--- code/DIY3-esp32/testing/ds3231_battery_alarm/ds3231_battery_alarm.ino
+++ code/DIY3-esp32/testing/ds3231_battery_alarm/ds3231_battery_alarm.ino
@@ -1,12 +1,11 @@
 /*
- * This file contains code for experimenting with with a DS3231 module.
+ * This file contains code for experimenting with a DS3231 module.
  * The DS3231 module has been stripped down to just the DS3231, 2 capacitors,
  * and a coin cell holder. 
  * 
  * The program first initializes the DS3231 and gets the time. Then, the once per
- * second alarm is enabled on the DS3231 and used to trigger an interrupt on the 
- * ESP32. Finally, VCC power to the DS3231 is disabled to make sure it will 
- * continue to run on just a battery.
+ * second alarm is enabled on the DS3231 and used to trigger a deep sleep wakeup
+ * on the ESP32 repeatedly.
  */
 
 
@@ -17,21 +16,21 @@
 #define RTC_ALARM GPIO_NUM_25
 
 
-// Flag that will be set to 1 whenever an interrupt is observed.
-volatile boolean isInt = 0;
-
-void IRAM_ATTR interruptHandler() {
-    isInt = 1;
+// Deep sleep wake stub function that is automatically called first when
+// waking up from deep sleep.
+/*
+void IRAM_ATTR esp_wake_deep_sleep(void) {
+    esp_default_wake_deep_sleep();
     // Detach interrupts while we handle this one, otherwise we'll get stuck in
     // a neverending stream of interrupts.
     detachInterrupt(RTC_ALARM);
 }
-
+*/
 
 void setup() {
     Serial.begin(115200);
     Serial.println("Start");
-    delay(1000);
+    //delay(1000);
 
     pinMode(RTC_POWER_PIN, OUTPUT);
     // The DS3231's alarm is active low, so we need to pull it high.
@@ -47,41 +46,69 @@ void setup() {
     // This allows us to generate alarm pulses while the chip is powered only by
     // battery. Also set other default values.
     // TODO: Need a way to determine if this fails.
-    Wire.begin();
-    DS3231_init(DS3231_CONTROL_BBSQW | DS3231_CONTROL_RS2 | DS3231_CONTROL_RS1 | DS3231_CONTROL_INTCN);
-    // Clear any previous alarms.
-    DS3231_clear_a1f();
-    DS3231_clear_a2f();
-
-
-    // Print out the time as a sanity check.
     struct ts now;
-    DS3231_get(&now);
-    Serial.printf("%d-%02d-%02d %02d:%02d:%02d\n", now.year, now.mon, now.mday, now.hour, now.min, now.sec);
-
-    // Shut down DS3231's VCC and turn it back on to make sure
-    // restarting the chip works.
-    digitalWrite(RTC_POWER_PIN, LOW);
-    delay(1000);
-    digitalWrite(RTC_POWER_PIN, HIGH);
-    delay(5);
-    DS3231_get(&now);
-    Serial.printf("%d-%02d-%02d %02d:%02d:%02d\n", now.year, now.mon, now.mday, now.hour, now.min, now.sec); 
+    Wire.begin();
     
-    // Attach a simple interrupt to be triggered when the alarm activates
-    // (active low).
-    attachInterrupt(RTC_ALARM, interruptHandler, ONLOW); // ONLOW and ONHIGH, not LOW and HIGH
+    switch (esp_reset_reason()) {
+        case ESP_RST_POWERON: {
+            DS3231_init(DS3231_CONTROL_BBSQW | DS3231_CONTROL_RS2 | DS3231_CONTROL_RS1 | DS3231_CONTROL_INTCN);
+            // Clear any previous alarms.
+            DS3231_clear_a1f();
+            DS3231_clear_a2f();
 
-    // Set the alarm to repeat every second. The time provided doesn't matter.
-    Serial.println("Starting alarm...");
-    // TODO: Modify library to make this better
-    const uint8_t flags[] = {1,1,1,1,0};
-    DS3231_set_a1(1, 1, 1, 1, flags);
-    DS3231_set_creg(DS3231_CONTROL_BBSQW | DS3231_CONTROL_RS2 | DS3231_CONTROL_RS1 | DS3231_CONTROL_INTCN | DS3231_CONTROL_A1IE);
+            // Print out the time as a sanity check.
+            DS3231_get(&now);
+            Serial.printf("%d-%02d-%02d %02d:%02d:%02d\n", now.year, now.mon, now.mday, now.hour, now.min, now.sec);
+
+            // Shut down DS3231's VCC and turn it back on to make sure
+            // restarting the chip works.
+            digitalWrite(RTC_POWER_PIN, LOW);
+            delay(1000);
+            digitalWrite(RTC_POWER_PIN, HIGH);
+            delay(5);
+            DS3231_get(&now);
+            Serial.printf("%d-%02d-%02d %02d:%02d:%02d\n", now.year, now.mon, now.mday, now.hour, now.min, now.sec); 
+            
+            // Attach a simple interrupt to be triggered when the alarm activates
+            // (active low).
+            // attachInterrupt(RTC_ALARM, interruptHandler, ONLOW); // ONLOW and ONHIGH, not LOW and HIGH
+
+            esp_sleep_enable_ext0_wakeup(RTC_ALARM, 0);
+
+            // Set the alarm to repeat every second. The time provided doesn't matter.
+            Serial.println("Starting alarm...");
+            // TODO: Modify library to make this better
+            const uint8_t flags[] = {1,1,1,1,0};
+            DS3231_set_a1(1, 1, 1, 1, flags);
+            DS3231_set_creg(DS3231_CONTROL_BBSQW | DS3231_CONTROL_RS2 | DS3231_CONTROL_RS1 | DS3231_CONTROL_INTCN | DS3231_CONTROL_A1IE);
+            
+            esp_deep_sleep_start();
+
+            break; // switch
+        }
+
+        case ESP_RST_DEEPSLEEP: {
+            DS3231_clear_a1f();
+
+            // Print out the time as a sanity check.
+            DS3231_get(&now);
+            Serial.printf("%02d-%02d-%02d %02d:%02d:%02d\n", now.year, now.mon, now.mday, now.hour, now.min, now.sec);
+
+            esp_sleep_enable_ext0_wakeup(RTC_ALARM, 0);
+            esp_deep_sleep_start();
+
+            break; // switch
+        }
+
+        default: {
+            Serial.println("DONE");
+        }
+    }
 }
 
-int count = 0;
-void loop() {
+
+void loop() {}
+/*
     // Print a message whenever there is an interrupt
     if (isInt) {
         // Clear the alarm signal so we don't instantly trigger another 
@@ -105,4 +132,4 @@ void loop() {
         Serial.println("OFF");
         count++;
     }
-}
+}*/
