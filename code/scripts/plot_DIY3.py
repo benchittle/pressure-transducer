f7458aef956b4c99315f01adefc71faeaca2d769
Ben Chittle <benchittle@me.com>
2022-05-31 21:51:18 -0400

    Add code for reading RBR and experimenting with data

--- code/scripts/plot_DIY3.py
+++ code/scripts/plot_DIY3.py
@@ -1,3 +1,4 @@
+# %%
 import pandas as pd
 from datetime import datetime
 import os
@@ -7,30 +8,51 @@ INPATH = "/home/benc/Documents/DIY3_out/C401-E15D"
 
 allData = {}
 
+def read_diy3(path, sensorID):
+     # Read data and rename columns, using timestamp as the index.
+    data = pd.read_csv(
+        filepath_or_buffer=path, 
+        index_col="timestamp", 
+        parse_dates=True
+    )
+    data.rename(columns={"pressure" : sensorID + "_P", "temperature" : sensorID + "_T"}, inplace=True)
+    return data
 
+def read_rbr(path, sensorID):
+    # Read data and rename columns, using timestamp as the index.
+    data = pd.read_excel(
+        io=path,
+        skiprows=5, 
+        usecols=[0, 1, 2], # Select Timestamp, Pressure, and Temperature
+        names=["timestamp", sensorID + "_T", sensorID + "_P", ]) # Rename columns
+    # Convert time datetime objects
+    data["timestamp"] = pd.to_datetime(data["timestamp"], dayfirst=True)
+    data.set_index("timestamp", inplace=True)
+    # Convert pressure to mbar
+    data[sensorID + "_P"] *= 100 
+
+    return data
+
+# %%
 for f in os.listdir(INPATH):
     name, ext = os.path.splitext(f)
-    if ext != ".csv":
+    # Read DIY3 data
+    if ext == ".csv" and "_" in name:
+        sensorID, timestamp = name.split("_")# Select Timestamp and Pressure columns
+        #timestamp = datetime.strptime(timestamp, r"%Y%m%d-%H%M")
+        data = read_diy3(INPATH + "/" + f, sensorID)
+    # Read RBR data
+    elif ext == ".xls":
+        sensorID = "RBR"
+        data = read_rbr(INPATH + "/" + f, sensorID)
+    else:
         continue
-    
-    sensorID, timestamp = name.split("_")
-    timestamp = datetime.strptime(timestamp, r"%Y%m%d-%H%M")
-
-    # Read just the pressure data and use timestamp as the index
-    data = pd.read_csv(INPATH + "/" + f, index_col="timestamp")
-    # Rename pressure columns to be unique to each sensor
-    data.rename(columns={"pressure":sensorID + "_P", "temperature":sensorID + "_T"}, inplace=True)
     
     # Add each data file to a list for that sensor
     if sensorID in allData:
-        allData[sensorID].append((timestamp, data))
+        allData[sensorID].append(data)
     else:
-        allData[sensorID] = [(timestamp, data)]
-
-# Sort each sensor's data (kinda)
-for key, lst in allData.items():
-    lst.sort(key=lambda x: x[0])
-    allData[key] = [x[1] for x in lst]
+        allData[sensorID] = [data]
 
 # Concatenate each sensor's data into a single dataframe per sensor
 allData = {sensor : pd.concat(allData[sensor]) for sensor in allData}
@@ -38,13 +60,22 @@ allData = {sensor : pd.concat(allData[sensor]) for sensor in allData}
 # Concatenate all the data into a single dataframe.
 allData = pd.concat(allData.values(), axis=1).sort_index()
 allData = allData.reindex(sorted(allData.columns), axis=1)
-print(allData)
+allData[[c for c in allData.columns if c.endswith("_P")]].plot()
+#pyplot.show()
+
+# %%
+#temp = allData.copy()
+#temp["RBR_P"] = temp["RBR_P"].fillna(method="ffill")
+#temp[[c for c in temp.columns if c.endswith("_P")]].plot(rot=45)
+#allData.to_csv(f"{INPATH}/sampleData.csv")
+for c in allData.columns:
+    if c.endswith("_P"):
+        allData[c].plot()
+        print(c)
+        pyplot.show()
 
 
-allData[[c for c in allData.columns if c.endswith("_P")]].plot(rot=45)
-allData.to_csv(f"{INPATH}/sampleData.csv")
-pyplot.show()
 
 
 
-
+# %%
