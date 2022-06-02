38fe896a2daecce45249cad9d27eb136c7af27e9
Ben Chittle <benchittle@me.com>
2022-06-02 17:29:22 -0400

    Reorganize and modularize script

--- code/scripts/plot_data.py
+++ code/scripts/plot_data.py
@@ -5,7 +5,6 @@ from matplotlib import pyplot
 
 INPATH = "/home/benc/Documents/DIY3_out/test run 1"
 
-allData = {}
 
 def read_diy3(path, sensorID):
      # Read data and rename columns, using timestamp as the index.
@@ -14,7 +13,12 @@ def read_diy3(path, sensorID):
         index_col="timestamp", 
         parse_dates=True
     )
-    data.rename(columns={"pressure" : sensorID + "_P", "temperature" : sensorID + "_T"}, inplace=True)
+    data.rename(
+        columns={
+            "pressure" : sensorID + "_P", 
+            "temperature" : sensorID + "_T"}, 
+        inplace=True
+    )
     
     return data
 
@@ -38,32 +42,61 @@ def read_rbr(path, sensorID):
 
     return data
 
-for f in os.listdir(INPATH):
-    name, ext = os.path.splitext(f)
-    # Read DIY3 data
-    if ext == ".csv" and "_" in name:
-        sensorID, timestamp = name.split("_")# Select Timestamp and Pressure columns
-        #timestamp = datetime.strptime(timestamp, r"%Y%m%d-%H%M")
-        data = read_diy3(INPATH + "/" + f, sensorID)
-    # Read RBR data
-    elif ext == ".txt":
-        sensorID = "RBR"
-        data = read_rbr(INPATH + "/" + f, sensorID)
-    else:
-        continue
-    
-    # Add each data file to a list for that sensor
-    if sensorID in allData:
-        allData[sensorID].append(data)
-    else:
-        allData[sensorID] = [data]
+# Read all DIY3 and RBR data in the given directory into a DataFrame.
+def read_data(dir_path):
+    all_data = {}
+    for f in os.listdir(dir_path):
+        name, ext = os.path.splitext(f)
+        # Read DIY3 data
+        if ext == ".csv" and "_" in name:
+            sensorID, timestamp = name.split("_")
+            #timestamp = datetime.strptime(timestamp, r"%Y%m%d-%H%M")
+            data = read_diy3(dir_path + "/" + f, sensorID)
+        # Read RBR data
+        elif ext == ".txt" and name.endswith("eng"):
+            sensorID = "RBR"
+            data = read_rbr(dir_path + "/" + f, sensorID)
+        else:
+            continue
+        
+        # Add each data file to a list for that sensor
+        if sensorID in all_data:
+            all_data[sensorID].append(data)
+        else:
+            all_data[sensorID] = [data]
 
-# Concatenate each sensor's data into a single dataframe per sensor
-allData = {sensor : pd.concat(allData[sensor]) for sensor in allData}
+    # Concatenate each sensor's data into a single dataframe per sensor
+    all_data = {id : pd.concat(all_data[id]) for id in all_data}
 
-# Concatenate all the data into a single dataframe.
-allData = pd.concat(allData.values(), axis=1).sort_index()
-allData = allData.reindex(sorted(allData.columns), axis=1)
-allData[[c for c in allData.columns if c.endswith("_P")]].plot()
-pyplot.show()
+    # Concatenate all the data into a single dataframe.
+    all_data = pd.concat(all_data.values(), axis=1).sort_index()
+    all_data = all_data.reindex(sorted(all_data.columns), axis=1)
 
+    return all_data
+
+
+# Plot all pressure data together
+def plot_pressure(all_data):
+    all_data[[c for c in all_data.columns if c.endswith("_P")]].plot()
+    pyplot.show()
+
+
+# Plot all pressure data but only show one at a time.
+def plot_pressure_individually(all_data):
+    for c in all_data.columns:
+        if c.endswith("_P"):
+            all_data[c].plot()
+            pyplot.show()
+
+
+# Plot all temperature data together.
+def plot_temperature(all_data):
+    all_data[[c for c in all_data.columns if c.endswith("_T")]].plot()
+    pyplot.show()
+
+
+def main():
+    sensor_data = read_data(INPATH)
+
+if __name__ == "__main__":
+    main()
