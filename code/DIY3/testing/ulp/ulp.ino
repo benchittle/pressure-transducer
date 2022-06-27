765077d5f395df8108bdf21cb87488bdb7840282
Ben Chittle <benchittle@me.com>
2022-06-27 17:06:15 -0400

    Update to read from sensor and store in buffer

--- code/DIY3/testing/ulp/ulp.ino
+++ code/DIY3/testing/ulp/ulp.ino
@@ -7,51 +7,40 @@
 #include "ds3231.h"
 #include "MS5803_05.h"
 
+#define RTC_ALARM GPIO_NUM_25
+
 
 // ESP32 I2C monitoring connections
 #define ESP_SDA GPIO_NUM_21
 #define ESP_SCL GPIO_NUM_22
 
 
-// first send CMD_ADC_CONV + CMD_ADC_Dx + CMD_ADC_4096
-// then wait 10ms
-// then 
-
-
 // ULP I2C connections
 #define SCL_PIN GPIO_NUM_14 
 #define SDA_PIN GPIO_NUM_13
 
-#define LED_PIN SDA_PIN
-
-
-#define M_I2C_DATA_HI() I_GPIO_OUTPUT_DIS(SDA_PIN)
-#define M_I2C_DATA_LO() \
-    I_GPIO_OUTPUT_EN(SDA_PIN), \
-    I_GPIO_SET(SDA_PIN, 0)
-
-#define M_I2C_CLOCK_HI() I_GPIO_OUTPUT_DIS(SCL_PIN)
-#define M_I2C_CLOCK_LO() \
-    I_GPIO_OUTPUT_EN(SCL_PIN), \
-    I_GPIO_SET(SCL_PIN, 0)
-
-
-#define M_I2C_DELAY() M_DELAY_US_100_5000(5000)
+RTC_DATA_ATTR ulp_var_t ulp_buffer[50];
+RTC_DATA_ATTR ulp_var_t ulp_buf_offset;
+RTC_DATA_ATTR ulp_var_t ulp_D2_flag;
 
 RTC_DATA_ATTR ulp_var_t ulp_var1;
 RTC_DATA_ATTR ulp_var_t ulp_var2;
 
-RTC_DATA_ATTR ulp_var_t ulp_read_cmd[HULP_I2C_CMD_BUF_SIZE(3)] = {
+RTC_DATA_ATTR ulp_var_t ulp_i2c_read_sensor[HULP_I2C_CMD_BUF_SIZE(3)] = {
     //HULP_I2C_CMD_HDR_NO_PTR(MS5803_I2C_ADDRESS, 2)
     HULP_I2C_CMD_HDR_NO_PTR(MS5803_I2C_ADDRESS, 3)
 };
 
 // TODO: Why does NO_PTR cause the command to become a read?
-RTC_DATA_ATTR ulp_var_t ulp_write_startconv_cmd[] = {
+RTC_DATA_ATTR ulp_var_t ulp_i2c_write_convertD1[] = {
     HULP_I2C_CMD_HDR(MS5803_I2C_ADDRESS, 0x48, 0),
-    //HULP_I2C_CMD_1B(0b10100000)
 };
-RTC_DATA_ATTR ulp_var_t ulp_write_read_cmd[] = {
+
+RTC_DATA_ATTR ulp_var_t ulp_i2c_write_convertD2[] = {
+    HULP_I2C_CMD_HDR(MS5803_I2C_ADDRESS, 0x58, 0),
+};
+
+RTC_DATA_ATTR ulp_var_t ulp_i2c_write_readADC[] = {
     HULP_I2C_CMD_HDR(MS5803_I2C_ADDRESS, 0x0, 0),
     //HULP_I2C_CMD_1B(0b10100000)
 };
@@ -65,10 +54,6 @@ uint8_t sdaVals[256];
 uint8_t count = 0;
 
 uint32_t varD1, varD2;
-
-uint8_t creg = 0;
-uint8_t sdaVal, sclVal;
-
 
 // addr: 0x77 = 0b1110111
 /*
@@ -86,6 +71,8 @@ MS_5803 sensor(4096);
 void init_ulp()
 {
     enum {
+        L_LOOP_FOR_D2,
+
         L_READ,
         L_WRITE,
         L_W_RETURN0,
@@ -95,29 +82,64 @@ void init_ulp()
 
 
     const ulp_insn_t program[] = {
-        M_I2C_DELAY(),
-
         
-        I_MOVO(R1, ulp_write_startconv_cmd),
+        // Check to see if the RTC alarm was triggered. If so, continue. 
+        // Otherwise, halt and put the ULP back to sleep until the next cycle.
+        I_GPIO_INT_RD(RTC_ALARM),
+        I_BGE(2, 1), // Skip halt if triggered
+        I_HALT(),
+        
+        // Clear the interrupt that was triggered
+        I_GPIO_INT_CLR(RTC_ALARM),
+
+        // Clear D2 read flag
+        I_MOVI(R2, 0),
+        I_MOVI(R0, 0),
+        I_PUT(R0, R2, ulp_D2_flag),
+        
+        // Issue I2C command to MS5803 to start ADC conversion
+        I_MOVO(R1, ulp_i2c_write_convertD1),
+        M_LABEL(L_LOOP_FOR_D2),     // Loop back here for D2 after D1
         M_MOVL(R3, L_W_RETURN0),
         M_BX(L_WRITE),
         M_LABEL(L_W_RETURN0),
         
-        M_DELAY_US_5000_20000(15000),
+        // Wait 10ms for conversion
+        M_DELAY_US_5000_20000(10000),
 
-        I_MOVO(R1, ulp_write_read_cmd),
+        // Issue I2C command to prepare to read the ADC value
+        I_MOVO(R1, ulp_i2c_write_readADC),
         M_MOVL(R3, L_W_RETURN1),
         M_BX(L_WRITE),
         M_LABEL(L_W_RETURN1),
 
-        M_DELAY_US_100_5000(1000),
-
-        I_MOVO(R1, ulp_read_cmd),
+        // Read from sensor using I2C.
+        I_MOVO(R1, ulp_i2c_read_sensor),
         M_MOVL(R3, L_R_RETURN),
         M_BX(L_READ),
         M_LABEL(L_R_RETURN),
 
+        // Store value in buffer
+        I_MOVI(R2, 0),
+        I_GET(R3, R2, ulp_buf_offset),
+        I_GET(R0, R2, ulp_i2c_read_sensor[HULP_I2C_CMD_DATA_OFFSET]),
+        I_GET(R1, R2, ulp_i2c_read_sensor[HULP_I2C_CMD_DATA_OFFSET + 1]),
+        I_PUTO(R0, R3, 0, ulp_buffer),
+        I_PUTO(R1, R3, -1, ulp_buffer),
+        I_ADDI(R3, R3, 2),
+        I_PUT(R3, R2, ulp_buf_offset),
 
+        // Loop back and repeat for D2. If D2 was just read, jump past next block.
+        I_GET(R0, R2, ulp_D2_flag),
+        I_BGE(5, 1),
+
+        // Set D2 flag before branching to get D2.
+        I_MOVI(R0, 1),
+        I_PUT(R0, R2, ulp_D2_flag),
+        I_MOVO(R1, ulp_i2c_write_convertD2),
+        M_BX(L_LOOP_FOR_D2),
+
+        // Branch here if we've already read D2.
         I_END(),
         I_HALT(),
 
@@ -130,6 +152,7 @@ void init_ulp()
 
     ESP_ERROR_CHECK(hulp_configure_pin(SCL_PIN, RTC_GPIO_MODE_INPUT_ONLY, GPIO_FLOATING, 0));
     ESP_ERROR_CHECK(hulp_configure_pin(SDA_PIN, RTC_GPIO_MODE_INPUT_ONLY, GPIO_FLOATING, 0));   
+    ESP_ERROR_CHECK(hulp_configure_pin_int(RTC_ALARM, GPIO_INTR_LOW_LEVEL));
 
     hulp_peripherals_on();
 
@@ -146,39 +169,35 @@ void setup() {
     Serial.println("START");
 
     ulp_var1.val = 0;
+    ulp_buf_offset.val = 0;
+    ulp_D2_flag.val = 0;
 
-    //pinMode(LED_PIN, OUTPUT);
-    
-
-    //hulp_configure_pin(LED_PIN, RTC_GPIO_MODE_OUTPUT_ONLY, GPIO_FLOATING, 1);
     hulp_peripherals_on();
 
     pinMode(ESP_SDA, INPUT);
     pinMode(ESP_SCL, INPUT);
     
-    
-    //Wire.begin();
-    //DS3231_init(DS3231_CONTROL_BBSQW | DS3231_CONTROL_RS2 | DS3231_CONTROL_RS1 | DS3231_CONTROL_INTCN);
-    //creg = DS3231_get_creg();
-    //printf("start var1: %d\nstart creg: %x\n\n", ulp_var1.reg_off, creg);
-
     sensor.initializeMS_5803(true);
     sensor.readSensor();
     printf("D1: %x \tD2: %x\n", sensor.D1val(), sensor.D2val());
     printf("P: %f \tT: %f\n", sensor.pressure(), sensor.temperature());
 
-    
-
     init_ulp();
-    //delay(1500);
-
+    
     
     while (!digitalRead(ESP_SCL) || !digitalRead(ESP_SDA));
-    //delay(250);
+    
     monitor();
     for (int i = 0; i < count && i < sizeof(sdaVals); i++) {
-        printf("(%d) SDA: %d\n", i, sdaVals[i]);
+        //printf("(%d) SDA: %d\n", i, sdaVals[i]);
     }
+
+    delay(1000);
+    printf("Offset: %d\n", ulp_buf_offset.val);
+        for (int i = 0; i < 4; i++) {
+        printf("D%d: %x\n", i / 2 + 1, ulp_buffer[i].val);
+    }
+    
 }
 
 
@@ -186,7 +205,7 @@ void setup() {
 //                                              read[offset + 1] >> 8 = lower byte
 void loop() {
     
-    printf("v0: %x \tv1: %x\n\n", ulp_read_cmd[HULP_I2C_CMD_DATA_OFFSET].val, ulp_read_cmd[HULP_I2C_CMD_DATA_OFFSET + 1].val);
+    printf("buf: %x \tcount: %d\n\n", ulp_buffer[ulp_buf_offset.val / 4].val, ulp_buf_offset.val);
     delay(1000);   
     
 }
@@ -204,15 +223,18 @@ void monitor() {
         count++;
         time_t t1 = millis();
         while(digitalRead(ESP_SCL)) {
+            
             if (millis() - t1 > 4000) {
                 return;
             }
+            /*
             if (sdaVal && !digitalRead(ESP_SDA) && !flag) {
                 //printf("START COND\n\n");
                 sdaVals[count] = 2;
                 count++;
                 flag = 1;
             }
+            */
         }
         t1 = millis();
         while(!digitalRead(ESP_SCL)) {
